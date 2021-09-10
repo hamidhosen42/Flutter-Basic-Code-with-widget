c721ebf50fa0f434912986333fb974aeda89798b
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-09-10 08:45:02 +0600

    Update 6-Mediaquery for responsive design flutter.dart

--- 6-Mediaquery for responsive design flutter.dart
+++ 6-Mediaquery for responsive design flutter.dart
@@ -19,14 +19,17 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context){
     //var height=MediaQuery.of(context).size.height;
     //var container_height=height/4;
+    var width=MediaQuery.of(context).size.width;
+    var container_width=width/3;
     return SafeArea(
         child: Scaffold(
           body: Center(
             child: Container(
               height:MediaQuery.of(context).size.height/4,
               //height:container_height,
-              width: MediaQuery.of(context).size.width/3,
-              color: Colors.yellow,
+              //width: MediaQuery.of(context).size.width/3,
+              width: container_width,
+              color: Colors.red,
             ),
           ),
         ),
